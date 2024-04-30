5538ce851aadbaf5c0c50b4286537a5bb188ca94
Leandro A. Siqueira <leandro.siqueira@venturus.org.br>
2024-04-30 17:11:57 -0300

    feature: Github actions

---
+++ src/App.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
+import App from './App';
+
+describe('A truthy statement', () => {
+    test('True to be true', () => {
+        expect(true).toBe(true);
+    });
+
+    test('should render headline', () => {
+        render(<App />);
+        expect(screen.getByText('Vite + React')).toBeInTheDocument();
+    });
+});
---
+++ src/components/Preloader.tsx
@@ -0,0 +1,9 @@
+function Preloader() {
+    return (
+        <div id="preloader" style={{ display: 'none' }}>
+            <div id="loader" style={{ display: 'none' }}></div>
+        </div>
+    );
+}
+
+export default Preloader;
